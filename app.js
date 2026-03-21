ee5b9b5c5438d9296b60b909323b6b805bc9c6dc
budhathoki10 <budhathokikushal170@gmail.com>
2026-03-21 15:44:41 +0545

    first commit

---
+++ app.js
@@ -0,0 +1,10 @@
+const express= require("express")
+const app= express();
+
+app.post("/",(req,res)=>{
+    res.status(200).json("hello world");
+})
+app.listen(5000,()=>{
+    console.log(` http://localhost:5000`)
+    console.log("server connected sucessfully")
+})
