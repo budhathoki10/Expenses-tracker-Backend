882fd955a3286f2ca2c7e836ac6c5de64118264e
budhathoki10 <budhathokikushal170@gmail.com>
2026-03-21 15:52:20 +0545

    tested if node module is commited or not

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const app= express();
 app.post("/",(req,res)=>{
     res.status(200).json("hello world");
 })
+
 app.listen(5000,()=>{
     console.log(` http://localhost:5000`)
     console.log("server connected sucessfully")
