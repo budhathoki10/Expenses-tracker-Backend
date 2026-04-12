20cb25088b9eb98960a2d324e159d2dfb0a2fd1d
budhathoki10 <budhathokikushal170@gmail.com>
2026-04-12 19:28:41 +0545

    brevo

--- utils/nodemailer.utils.js
+++ utils/nodemailer.utils.js
@@ -1,87 +1,24 @@
-// const transporter = require("../Nodemailer/sendemail.Nodemailer");
-// const otpEmailTemplate = require("../Email/OTPtemplate.Email");
-// const sendEmail = async (email, subject, content) => {
-//   try {
-//     console.log("sending emailto",email)
-//     const template = {
-//       from: process.env.EMAIL_USER,
-//       to: email,
-//       subject: subject,
-//       html: content,
-//     };
-//     // console.log("SMTP user:", process.env.EMAIL_USER);
-//     // console.log("SMTP pass:", process.env.EMAIL_PASS);
+const Brevo = require('@getbrevo/brevo');
 
-//     console.log("before");
-//     await transporter.sendMail(template);
-//     // console.log("Email sent:", info);
+const client = Brevo.ApiClient.instance;
+client.authentications['api-key'].apiKey = process.env.BREVO_API_KEY;
 
-//     console.log("sended email");
-//     return { success: true, message: "email send sucessfully" };
-//   } catch (error) {
-//     console.error("send email error:", error);
-//     return { success: false, message: "internal server error" };
-//   }
-// };
+const apiInstance = new Brevo.TransactionalEmailsApi();
 
-
-// const { Resend } = require('resend');
-// const resend = new Resend(process.env.RESEND_API_KEY);
-
-// const sendEmail = async (email, subject, content) => {
-//   try {
-//     const data = await resend.emails.send({
-//       from: 'onboarding@resend.dev',   // free sender
-//       to: email,                      // 👈 any user's email
-//       subject: subject,
-//       html: content,
-//     });
-//     console.log('Email sent:', data);
-//   } catch (error) {
-//     console.error('Email error:', error);
-//   }
-// };
-
-// const sendPersonalEmail = async (email, subject, content) => {
-//   try {
-//     // console.log("sending emailto",email)
-//     const template = {
-//       from: email,
-//       to: process.env.EMAIL_USER,
-//       subject: subject,
-//       html: content,
-//     };
-//     // console.log("SMTP user:", process.env.EMAIL_USER);
-//     // console.log("SMTP pass:", process.env.EMAIL_PASS);
-
-//     console.log("before");
-//     await transporter.sendMail(template);
-//     // console.log("Email sent:", info);
-
-//     console.log("sended email");
-//     return { success: true, message: "email send sucessfully" };
-//   } catch (error) {
-//     console.error("send email error:", error);
-//     return { success: false, message: "internal server error" };
-//   }
-// };
-
-// module.exports = {sendEmail,sendPersonalEmail};
-
-
-const { Resend } = require('resend');
-const resend = new Resend(process.env.RESEND_API_KEY);
 
 const sendEmail = async (email, subject, content) => {
   try {
     console.log("sending email to", email);
-    const data = await resend.emails.send({
-      from: 'budhathokikushal170@gmail.com',
-      to: email,
+    await apiInstance.sendTransacEmail({
+      sender: { 
+        email: process.env.ADMIN_EMAIL,  
+        name: 'Expenses Tracker' 
+      },
+      to: [{ email: email }],          
       subject: subject,
-      html: content,
+      htmlContent: content,
     });
-    console.log('Email sent:', data);
+    console.log('Email sent successfully');
     return { success: true, message: "email sent successfully" };
   } catch (error) {
     console.error('send email error:', error);
@@ -92,14 +29,17 @@ const sendEmail = async (email, subject, content) => {
 const sendPersonalEmail = async (email, subject, content) => {
   try {
     console.log("sending personal email from", email);
-    const data = await resend.emails.send({
-      from: 'onboarding@resend.dev',
-      to: process.env.EMAIL_USER,
+    await apiInstance.sendTransacEmail({
+      sender: { 
+        email: email, 
+        name: 'Expenses Tracker' 
+      },
+      to: [{ email: process.env.ADMIN_EMAIL }],  
       subject: subject,
-      html: content,
-      replyTo: email,
+      htmlContent: content,
+      replyTo: { email: email },                
     });
-    console.log('Personal email sent:', data);
+    console.log('Personal email sent successfully');
     return { success: true, message: "email sent successfully" };
   } catch (error) {
     console.error('send personal email error:', error);
