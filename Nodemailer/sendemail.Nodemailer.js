f69a774a12e116669f2bec3f639f25d8ff7afc96
budhathoki10 <budhathokikushal170@gmail.com>
2026-04-12 16:44:10 +0545

    fixes otp

--- Nodemailer/sendemail.Nodemailer.js
+++ Nodemailer/sendemail.Nodemailer.js
@@ -21,9 +21,6 @@ const transporter = nodemailer.createTransport({
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS
-  },
-  tls: {
-    rejectUnauthorized: false  // ← add this!
   }
 });
 
