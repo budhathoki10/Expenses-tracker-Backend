4701aa828b5242e01881fab99531ca60cb16a86f
budhathoki10 <budhathokikushal170@gmail.com>
2026-04-12 16:55:35 +0545

    fixes otp

--- Nodemailer/sendemail.Nodemailer.js
+++ Nodemailer/sendemail.Nodemailer.js
@@ -18,6 +18,7 @@ const transporter = nodemailer.createTransport({
   host: "smtp.gmail.com",
   port: 587,
   secure: false,
+  family: 4, // ✅ FORCE IPv4 (THIS FIXES YOUR ERROR)
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS
